7b5154a71ec6731b3c8cb62fcc68e7306c4ee017
Markus Blaschke <markus@familie-blaschke.net>
2017-09-19 15:55:15 +0200

    Update k8s-provision.sh

--- extensions/k8s-provision/v1/k8s-provision.sh
+++ extensions/k8s-provision/v1/k8s-provision.sh
@@ -7,12 +7,20 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 
 export DEBIAN_FRONTEND=noninteractive
 K8S_RUNTIME_CONFIG="api/all=true"
+VNET_RG="f-we-core-vnet-rg"
 
 # Enable alpha features
 if [[ -f /etc/kubernetes/manifests/kube-apiserver.yaml ]]; then
     JSONSP='        '
     sed -i '/\s*- "--runtime-config=.*$/d' /etc/kubernetes/manifests/kube-apiserver.yaml
     sed -i "/${JSONSP}- \"apiserver\"/c\\${JSONSP}- \"apiserver\"\n${JSONSP}- \"--runtime-config=$K8S_RUNTIME_CONFIG\"" /etc/kubernetes/manifests/kube-apiserver.yaml
+fi
+
+# Set Vnet resource group
+if [[ -f /etc/kubernetes/azure.json ]]; then
+    JSONSP='    '
+    sed -i '/\s*"VnetResourceGroup":.*$/d' /etc/kubernetes/azure.json
+    sed -i "/${JSONSP}\"cloud\":\"AzurePublicCloud\",/c\\${JSONSP}\"cloud\":\"AzurePublicCloud\",\n${JSONSP}\"VnetResourceGroup\":\"${VNET_RG}\"," /etc/kubernetes/azure.json
 fi
 
 # Kernel config
